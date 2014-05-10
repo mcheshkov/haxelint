db062e5f1eba17aabd5c20ead5c67df0288a57d1
Mikhail Cheshkov <muuuuuu@mail.ru>
2014-05-11 02:15:09 +0700

    Implement directory traversing

--- src/haxelint/Main.hx
+++ src/haxelint/Main.hx
@@ -1,5 +1,6 @@
 package haxelint;
 
+import sys.FileSystem;
 import haxelint.reporter.XMLReporter;
 import haxelint.reporter.Reporter;
 import haxe.CallStack;
@@ -9,15 +10,39 @@ class Main {
 		try{
 			if (Sys.args().length != 1) throw "Bad args count";
 			var fname = Sys.args()[0];
+			var files:Array<String> = [];
+			traverse(fname, files);
 
-			var code = File.getContent(fname);
+			var toProcess:Array<LintFile> = [];
+			for (file in files){
+				var code = File.getContent(file);
+				toProcess.push({name:file,content:code});
+			}
+
 			var checker = new Checker();
 			checker.addReporter(new XMLReporter());
-			checker.process([{name:fname,content:code}]);
+			checker.process(toProcess);
 		}
 		catch(e:Dynamic){
 			trace(e);
 			trace(CallStack.toString(CallStack.exceptionStack()));
+		}
+	}
+
+	private static function pathJoin(s:String, t:String):String{
+		//FIXME
+		return s + "/" + t;
+	}
+
+	private static function traverse(node:String , files:Array<String>) {
+		if (FileSystem.isDirectory(node)){
+			var nodes = FileSystem.readDirectory(node);
+			for (child in nodes){
+				traverse(pathJoin(node,child),files);
+			}
+		}
+		else if (node.substr(-3) == ".hx"){
+			files.push(node);
 		}
 	}
 }
