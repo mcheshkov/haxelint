09b137213931ac78c267eb682efadca8ef537231
Mikhail Cheshkov <muuuuuu@mail.ru>
2014-09-06 03:06:00 +0700

    Try to use haxelib install build.hxml in travis

--- .travis/before_install.sh
+++ .travis/before_install.sh
@@ -4,7 +4,4 @@ sudo apt-get update &&
 sudo apt-get install haxe -y &&
 mkdir ~/haxelib &&
 haxelib -notimeout setup ~/haxelib &&
-haxelib -notimeout install hxparse    # This is bad, but $ haxelib -notimeout install build.hxml
-haxelib -notimeout install haxeparser # requires user interaction FOR GODS SAKE
-haxelib -notimeout install haxeparser-substituted
-haxelib -notimeout install compiletime
+echo y | haxelib -notimeout install build.hxml
