27ddbf8826665c966d97e183a57650e6a91396d9
Mikhail Cheshkov <muuuuuu@mail.ru>
2014-10-10 11:29:04 +0700

    Add basic argument handling using hxargs

--- src/haxelint/Main.hx
+++ src/haxelint/Main.hx
@@ -1,10 +1,13 @@
 package haxelint;
 
+import haxelint.reporter.IReporter;
+import hxargs.Args;
 import sys.FileSystem;
 import haxelint.reporter.XMLReporter;
 import haxelint.reporter.Reporter;
 import haxe.CallStack;
 import sys.io.File;
+
 class Main {
 	public static function main() {
 		var args;
@@ -18,12 +21,25 @@ class Main {
 				cwd = args.pop();
 				oldCwd = Sys.getCwd();
 			}
-			if (args.length != 1) throw "Bad args count";
 			if (oldCwd != null) Sys.setCwd(cwd); // FIXME instead of this one should gracefully parse absolute-relative path
 
-			var fname = args[0];
+			var reporter:IReporter = new Reporter();
 			var files:Array<String> = [];
-			traverse(fname, files);
+
+			var argHandler = Args.generate([
+			@doc("Set reporter")
+			["-r", "--reporter"] => function(reporterName:String) reporter = createReporter(reporterName),
+			@doc("List all reporters")
+			["--list-reporters"] => function() listReporters(),
+			@doc("Set sources to process")
+			["-s", "--source"] => function(sourcePath:String) traverse(sourcePath,files),
+			_ => function(arg:String) throw "Unknown command: " + arg
+			]);
+			if (args.length == 0) {
+				Sys.println(argHandler.getDoc());
+				Sys.exit(0);
+			}
+			argHandler.parse(args);
 
 			var toProcess:Array<LintFile> = [];
 			for (file in files){
@@ -33,7 +49,7 @@ class Main {
 
 			var checker = new Checker();
 			checker.addAllChecks();
-			checker.addReporter(new XMLReporter());
+			checker.addReporter(reporter);
 			checker.process(toProcess);
 		}
 		catch(e:Dynamic){
@@ -41,6 +57,22 @@ class Main {
 			trace(CallStack.toString(CallStack.exceptionStack()));
 		}
 		if (oldCwd != null) Sys.setCwd(oldCwd); // FIXME instead of this one should gracefully parse absolute-relative path
+	}
+
+	//FIXME some macro maybe?
+	static function listReporters():Void {
+		Sys.println("default - Default reporter");
+		Sys.println("xml - Checkstyle-like XML reporter");
+		Sys.exit(0);
+	}
+
+	//FIXME some macro maybe?
+	static function createReporter(reporterName:String):IReporter{
+		return switch(reporterName){
+		case "xml": new XMLReporter();
+		case "default": new Reporter();
+		default: throw "Unknown reporter";
+		}
 	}
 
 	private static function pathJoin(s:String, t:String):String{
